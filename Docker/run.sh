a547873bc3497152ddb264c5aaac83763c74600b
shunpe-hash <d1301539@gmail.com>
2023-01-25 13:53:35 +0900

    update

--- Docker/run.sh
+++ Docker/run.sh
@@ -1,4 +1,4 @@
 docker run --gpus all --rm -it  \
     -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=$DISPLAY \
-    -v $HOME/ur3e-tutorial:/root/ur3e-tutorial \
+    -v $HOME/ur3e_tutorial:/root/ur3e_tutorial \
     ur3e-tutorial bash
