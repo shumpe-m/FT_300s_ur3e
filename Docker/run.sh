0fa44dd180fdc91c6f10cac053b08a07aea78b59
shunpe-hash <d1301539@gmail.com>
2023-02-24 14:40:08 +0900

    Modification

--- Docker/run.sh
+++ Docker/run.sh
@@ -1,7 +1,7 @@
-docker run --name ft_300s_ur3e --gpus all -rm -it  \
-    -u "$(id -u $USER):$(id -g $USER)" \
-    -v /etc/passwd:/etc/passwd:ro \
+docker run --name ft_300s_ur3e --gpus all --rm -it  \
+    -u $(id -u $USER):$(id -g $USER) \
     -v /etc/group:/etc/group:ro \
+    -v /etc/passwd:/etc/passwd:ro \
     -v /etc/shadow:/etc/shadow:ro \
     -v /etc/sudoers.d:/etc/sudoers.d:ro \
     -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=$DISPLAY \
